3e28750872941ac80cc3f5c26f3b4924d5ee4c82
Khoi Le <minarek@outlook.com>
2018-02-16 23:06:54 +0100

    Done JS-Mandatory1

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -26,51 +26,88 @@ function showShoppingCart(objProduct) {
     let parentElement = document.getElementById("tbShoppingCart");
     parentElement.appendChild(spItem);
 }
-function validationUserForm(){
-    let uFirstname = document.getElementById("txtFirstname");
-    let uLastname = document.getElementById("txtLastname");
-    let uEmail = document.getElementById("txtEmail");
-    let uPhoneNumber = document.getElementById("txtPhoneNumber");
-    let uAddress = document.getElementById("txtAddress");
-    let uZipCode = document.getElementById("txtZipCode");
-    let uCity = document.getElementById("txtCity");
-    let uCommnets = document.getElementById("txtComments");
-    let btnCheckout = document.getElementById("btnCheckout");
-
-    if (uFirstname.value === "" || uFirstname.value.length < 3 || uFirstname.value.length > 100 ){
-        alert("First name must be have and between 3-100 characters!");
-        uFirstname.focus();
-        return false;
-    }else if (uLastname.value === "" || uLastname.value.length < 3 || uLastname.value.length >100){
-        alert("Last name must be have and between 3-100 characters!");
-        uLastname.focus();
-        return false;
-    }
-}
-
-viewShoppingCart();
-document.getElementById("btnCheckout").addEventListener("click",validationUserForm);
-document.getElementById("txtPhoneNumber").addEventListener("keypress",function (event) {
-    validateNumber(event);
-});
-
-function validationKeyPress(event) {
+function limitInputLength(elementID,length) {
     let userKeyPress = window.event ? event.which : event.keyCode;
-    if (userKeyPress < 48 || userKeyPress > 57){
-        alert("no");
+
+    if (document.getElementById(elementID).value.length === length && userKeyPress !== 8){
+        event.preventDefault();
         return false;
     }else{
         return true;
     }
 }
+function limitKeyPressNumber() {
+    let userKeyPress = window.event ? event.which : event.keyCode;
 
-function validateNumber(event) {
-    var key = window.event ? event.keyCode : event.which;
-    if (event.keyCode === 8 || event.keyCode === 46) {
-        return true;
-    } else if ( key < 48 || key > 57 ) {
-        return false;
-    } else {
+    if (userKeyPress !== 8){
+        if (userKeyPress < 48 || userKeyPress > 57){
+            event.preventDefault(); //stop the key press
+            return false;
+        }
+    }else{
         return true;
     }
-};
+}
+function validationUserForm(){
+    if (uFirstname.value === "" || uFirstname.value.length < 3){
+        alert("First name must have and between 3-100 characters!");
+        uFirstname.focus();
+        return false;
+    }else if (uLastname.value === "" || uLastname.value.length < 3){
+        alert("Last name must have and between 3-100 characters!");
+        uLastname.focus();
+        return false;
+    }else if (uEmail.value === "" || uEmail.value.length < 5){
+        alert("Email must have and between 5-50 characters!");
+        uEmail.focus();
+        return false;
+    }else if(uPhoneNumber.value !== "" && uPhoneNumber.value.length < 6){
+        alert("Telephone number is optional and between 6-20 characters!");
+        uPhoneNumber.focus();
+        return false;
+    }else if(uAddress.value === "" || uAddress.value.length < 5){
+        alert("Street address must have and between 5-200 characters!");
+        uAddress.focus();
+        return false;
+    }else if(uZipCode.value === "" || uZipCode.value.length < 2){
+        alert("Zip code must have and between 2-10 characters!");
+        uZipCode.focus();
+        return false;
+    }else if(uCity.value === "" || uCity.value.length < 2){
+        alert("City must have and between 2-50");
+        uCity.focus();
+        return false;
+    }else{
+        alert("SUCCEED! Your order has been sent!");
+        return true;
+    }
+}
+
+//MAIN
+viewShoppingCart();
+
+//Form
+let uFirstname = document.getElementById("txtFirstname");
+let uLastname = document.getElementById("txtLastname");
+let uEmail = document.getElementById("txtEmail");
+let uPhoneNumber = document.getElementById("txtPhoneNumber");
+let uAddress = document.getElementById("txtAddress");
+let uZipCode = document.getElementById("txtZipCode");
+let uCity = document.getElementById("txtCity");
+let uCommnets = document.getElementById("txtComments");
+let btnCheckout = document.getElementById("btnCheckout");
+
+//Limit input length
+uFirstname.addEventListener("keypress",function () {limitInputLength("txtFirstname",100);});
+uLastname.addEventListener("keypress",function () {limitInputLength("txtLastname",100);});
+uEmail.addEventListener("keypress",function () {limitInputLength("txtEmail",50);});
+
+uPhoneNumber.addEventListener("keypress",limitKeyPressNumber);
+uPhoneNumber.addEventListener("keydown",function () {limitInputLength("txtPhoneNumber",20);});
+
+uAddress.addEventListener("keypress",function () {limitInputLength("txtAddress",200);});
+uZipCode.addEventListener("keypress",function () {limitInputLength("txtZipCode",10);});
+uCity.addEventListener("keypress",function () {limitInputLength("txtCity",50);});
+uCommnets.addEventListener("keypress",function () {limitInputLength("txtComments",1000);});
+
+btnCheckout.addEventListener("click",validationUserForm);
